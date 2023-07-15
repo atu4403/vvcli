3c77bd1d680ebd1b48a535283dbeeefb9ad57e19
atu4403 <73111778+atu4403@users.noreply.github.com>
2023-07-15 15:33:39 +0900

    refactor

--- src/vvcli/api.py
+++ src/vvcli/api.py
@@ -4,22 +4,29 @@ import requests
 # APIのurlを定数化する
 API_URL = "http://localhost:50021"
 
-def get_audio_query(speaker_id: int, text: str):
+def call_api(endpoint: str, params: dict = None, data: dict = None, http_method: str = "GET") -> requests.Response:
     # パラメータ付きのURLを作成する
-    url = f"{API_URL}/audio_query?text={text}&speaker={speaker_id}"
-    # POSTリクエストを送信する
-    response = requests.post(url)
+    url = f"{API_URL}/{endpoint}"
+    if params:
+        url += "?" + "&".join([f"{key}={value}" for key, value in params.items()])
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    # リクエストを送信する
+    response = requests.request(http_method, url, data=json.dumps(data), headers=headers)
     response.raise_for_status()
+    return response
+
+def get_audio_query(speaker_id: int, text: str):
+    # パラメータを設定する
+    params = {"text": text, "speaker": speaker_id}
+    # APIを呼び出す
+    response = call_api("audio_query", params=params, http_method="POST")
     audio_query = response.json()
-    assert type(audio_query) is dict
     return audio_query
 
 def get_wav_data(speaker_id: int, audio_query):
     # リクエストボディにaudio_queryとspeaker_idを設定する
-    data = json.dumps(audio_query)
-    headers = {"Content-Type": "application/json; charset=utf-8"}
-    # POSTリクエストを送信する
-    response = requests.post(f"{API_URL}/synthesis?speaker={speaker_id}", data=data, headers=headers)
-    response.raise_for_status()
+    data = audio_query
+    # APIを呼び出す
+    response = call_api(f"synthesis?speaker={speaker_id}", data=data, http_method="POST")
     wav_data = response.content
     return wav_data
