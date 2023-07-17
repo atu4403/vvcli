d51faaf760cb8081015eb3a8a3a77e8970cac9b3
atu4403 <73111778+atu4403@users.noreply.github.com>
2023-07-17 21:32:48 +0900

    add commands

--- src/vvcli/cli.py
+++ src/vvcli/cli.py
@@ -1,5 +1,7 @@
 import fire
+from pathlib import Path
 from .tools import text_to_speech
+from .api import Speakers
 
 
 def hello(name="World"):
@@ -13,5 +15,18 @@ def tts(speaker_id, text_file_path, audio_file_dir, max_length=1000, silent=Fals
     )
 
 
+def generate__speakers_html():
+    sp = Speakers()
+    sp.reformat_media()
+    sp.generate_html()
+
+
 def main():
-    fire.Fire({"hello": hello, "tts": tts, "text_to_speech": tts})
+    fire.Fire(
+        {
+            "hello": hello,
+            "tts": tts,
+            "text_to_speech": tts,
+            "generate__speakers_html": generate__speakers_html,
+        }
+    )
